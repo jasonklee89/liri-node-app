4fa5ff4a7f7a03a6b0ef6031f5f46518d42bf7d3
jason <jasonkellogglee@gmail.com>
2019-04-07 11:49:57 -0700

    finished concert-this

--- liri.js
+++ liri.js
@@ -1,17 +1,21 @@
 require("dotenv").config();
 var keys = require("./keys.js");
+var axios = require('axios');
+var moment = require('moment');
 // Access Spotify keys
 // var spotify = new Spotify(keys.spotify);
 var command = process.argv[2];
 var input = process.argv[3];
-var axios = require('axios');
 
 switch (command) {
     case 'concert-this':
-        var queryUrl = "https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp";
+        var queryUrl = 'https://rest.bandsintown.com/artists/' + input + '/events?app_id=codingbootcamp';
         console.log(queryUrl)
         axios.get(queryUrl).then(function(response) {
                 console.log("Venue: " + response.data[1].venue.name);
+                console.log("Location: " + response.data[1].venue.city);
+                var date = moment(response.data[1].datetime).format('MM/DD/YYYY');
+                console.log("Date: " + date);
             })
         break;
     case 'spotify-this-song':
